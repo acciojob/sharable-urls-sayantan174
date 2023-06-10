13ad5611f44258798e8c8c217e5e15c520d17a16
Sayantan Mandal <sayantanmandal174@gmail.com>
2023-06-10 10:23:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 // your code here
+const h3 = document.getElementById("url");
+h3.innerText = window.location.href;
+
+const form = document.getElementById("form");
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  h3.innerText = window.location.href;
+  if (form["name"].value !== "" && form["year"].value === "") h3.innerText += `?name=${form["name"].value}`;
+  if (form["year"].value !== "" && form["name"].value === "") h3.innerText += `?year=${form["year"].value}`;
+  if(form["year"].value !== "" && form["name"].value !== "")
+  h3.innerText += `?name=${form["name"].value}&year=${form["year"].value}`
+});
